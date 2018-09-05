72f46b212f233e51cca757cdef97e2b5653b3dde
Suman Astani <suman.astani@divyat.com>
2018-09-05 12:45:41 +0545

    setting changed for  database and requirement updated

--- toki/toki/settings.py
+++ toki/toki/settings.py
@@ -98,11 +98,14 @@ WSGI_APPLICATION = 'toki.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'toki',
+        'USER': 'simon',
+        'PASSWORD': 'simon',
+        'HOST': 'localhost',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',  # Set to empty string for default.
     }
 }
 
@@ -144,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/staticfiles/"
 
 # Crispy Form Theme - Bootstrap 3
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
